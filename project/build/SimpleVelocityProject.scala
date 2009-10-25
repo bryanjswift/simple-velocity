bb68bc5b00762d6195c5ff3ffa5b4e825ac82a2b
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-24 23:47:33 -0400

    Initial commit of velocity project

---
+++ project/build/SimpleVelocityProject.scala
@@ -0,0 +1,19 @@
+import sbt._
+
+class SimpleVelocityProject(info:ProjectInfo) extends DefaultProject(info) {
+	// *-- Compile Dependencies
+	// Servlet API
+	val servletApi = "javax.servlet" % "servlet-api" % "2.4"
+	// Velocity
+	val velocity = "org.apache.velocity" % "velocity" % "1.6.1"
+
+	// *-- Test Dependencies
+	// Specs
+	val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test->default"
+	val junit = "junit" % "junit" % "4.5" % "test->default"
+
+	// override looking for jars in ./lib
+	override def dependencyPath = "src" / "main" / "lib"
+	// override path to managed dependency cache
+	override def managedDependencyPath = "project" / "lib_managed"
+}
---
+++ src/test/scala/velocity/VelocityViewSpecs.scala
@@ -0,0 +1,62 @@
+package velocity
+
+import java.io.{PrintWriter,StringWriter}
+import org.specs.Specification
+
+object VelocityViewSpecs extends Specification {
+	class Request extends DummyRequest { }
+	class Response extends DummyResponse {
+		val writer = new StringWriter()
+		override def getWriter():PrintWriter = new PrintWriter(writer)
+    override def setCharacterEncoding(encoding:String) = { }
+	}
+	val objTestPath = "templates/objTest.vm"
+	val listTestPath = "templates/listTest.vm"
+
+	"Creating a VelocityView with a valid template path" should {
+		val view = new VelocityView(objTestPath)
+		"produce a view with a non-null template" >> {
+			view.template must notBeNull
+		}
+		"produce a non-empty context when passed a valid model" >> {
+			val context = view.createVelocityContext(Map("tests" -> List("list test")),null,null)
+			val keys = context.getKeys
+			keys.length mustEqual 1
+		}
+	}
+	"When rendering a view (" + objTestPath + ") with a simple object (obj), the view" should {
+		val view = new VelocityView(objTestPath)
+		val request = new Request()
+		val response = new Response()
+		val obj = new Object {
+			val test = "Tester.test"
+			override def toString = "Tester.toString"
+		}
+		view.render(Map("obj" -> obj),request,response)
+		val result = response.writer.toString
+		"contain the value of obj.toString" >> {
+			result indexOf obj.toString must beGreaterThanOrEqualTo(0)
+		}
+		"contain the value of obj.test" >> {
+			result indexOf obj.test must beGreaterThanOrEqualTo(0)
+		}
+	}
+	"When rendering a view (" + listTestPath + ") with a model containing a list, the view" should {
+		val view = new VelocityView(listTestPath)
+		val request = new Request()
+		val response = new Response()
+		val list = List("list test","list test2","another test")
+		view.render(Map("tests" -> list),request,response)
+		val result = response.writer.toString
+		"contain the first string" >> {
+			result must notEqualIgnoreSpace("")
+			result indexOf list(0) must beGreaterThanOrEqualTo(0)
+		}
+		"contain all of the strings" >> {
+			result must notEqualIgnoreSpace("")
+			list.foreach(s => {
+				result indexOf s must beGreaterThanOrEqualTo(0)
+			})
+		}
+	}
+}
