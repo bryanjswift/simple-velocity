60e589d9abafdf7289a13fee27dc9b61c555f00d
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-27 00:17:16 -0400

    Semantic changes for readbility's sake

--- src/main/scala/velocity/VelocityView.scala
+++ src/main/scala/velocity/VelocityView.scala
@@ -14,10 +14,10 @@ object VelocityHelper {
 
 class VelocityView(path:String) {
 	private class IterableWrapper[T](iterable:Iterable[T]) extends java.lang.Iterable[T] {
-		def iterator() = new java.util.Iterator[T] {
+		def iterator = new java.util.Iterator[T] {
 			private val delegate = iterable.elements
 			def hasNext = delegate.hasNext
-			def next() = delegate.next
+			def next = delegate.next
 			def remove = {
 				throw new UnsupportedOperationException
 			}
@@ -26,12 +26,12 @@ class VelocityView(path:String) {
 	val template:Template = VelocityHelper.getTemplate(path)
 	def createVelocityContext(model:Map[String,Any],request:Request,response:Response) = {
 		val context = new VelocityContext()
-		model.foreach(t => {
-			if (t._2.isInstanceOf[Iterable[_]])
-				context.put(t._1, new IterableWrapper(t._2.asInstanceOf[Iterable[_]]))
+		model.foreach(pair =>
+			if (pair._2.isInstanceOf[Iterable[_]])
+				context.put(pair._1, new IterableWrapper(pair._2.asInstanceOf[Iterable[_]]))
 			else
-				context.put(t._1, t._2)
-		})
+				context.put(pair._1, pair._2)
+		)
 		context
 	}
 	def render(model:Map[String,Any],request:Request,response:Response):Unit = {
