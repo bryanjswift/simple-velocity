1da7ddd9c2e4e4d5901611b2efb1b758364cea4c
Bryan J Swift <bryan.j.swift@gmail.com>
2010-11-17 18:47:08 -0500

    Use some scala 2.8.1 features to convert Java maps
    Bump to 0.3.2

--- src/main/scala/velocity/VelocityView.scala
+++ src/main/scala/velocity/VelocityView.scala
@@ -1,27 +1,35 @@
 package velocity
 
 import java.io.{PrintWriter, StringWriter}
+import java.lang.Object
 import javax.servlet.http.{HttpServletResponse => Response}
+import java.util.{Map => JMap}
 import org.apache.velocity.VelocityContext
 import scala.collection.JavaConversions
 
 class VelocityView(path:String) {
 	val template = VelocityHelper.getTemplate(path)
-	implicit def createVelocityContext(model:Map[String,Any]):VelocityContext = {
+	implicit def createVelocityContext(model:scala.collection.Map[String,Any]):VelocityContext = {
 		val context = new VelocityContext()
 		model.foreach(t => {
 			val (key,value) = t
 			val toPut = value match {
 				case s:Seq[_] =>
-					JavaConversions.asList(s)
+					JavaConversions.asJavaList(s)
 				case i:Iterable[_] =>
-					JavaConversions.asIterable(i)
+					JavaConversions.asJavaIterable(i)
 				case _ =>
 					value
 			}
 			context.put(key,toPut)
 		})
 		context
+	}
+	implicit def createVelocityContext(model:JMap[String,Object]):VelocityContext =
+		createVelocityContext(JavaConversions.asScalaMap(model))
+	def render(model:JMap[String,Object], response:Response):Unit = {
+		response.setCharacterEncoding("UTF-8")
+		template.merge(model, response.getWriter())
 	}
 	def render(model:Map[String,Any],response:Response):Unit = {
 		response.setCharacterEncoding("UTF-8")
